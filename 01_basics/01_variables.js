762a73c00b038fc7fb42b121a5040dd94f0d17c2
Vinay Thakur <thakurv69515@gmail.com>
2026-01-16 07:06:07 +0000

    dataTypes & variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId =144553 //this is constant we cann't change this
+let accountEmail ="vinayth11223344@gmail.com" //use this
+var accountPasswork = "1122334445" //never use var
+accountCity = "HP"
+accountState=
+console.log(accountId);
+console.table([accountId,accountPasswork,accountEmail,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as news version
+/*alert(3+3) we are using nodejs , not browser
+code readability should be high
+*/
+
+
+let name= "Vinay" //string
+let age =23 
+let isLoggedIn = false
+
+
+//primitive
+//number => 2 to power53 
+//bigInt used only when the big number is used
+// string => ""
+// boolean=> true/false
+// null => standalone value(representation of empty value)
+//undefined=> 
+// symbol => to find the uniqueness
+
+
+//non-primitive
+
+//objects
+
+console.log(typeof "Vinay")
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null)
+//the type of undefined is undefined where as null is an object
+
+
