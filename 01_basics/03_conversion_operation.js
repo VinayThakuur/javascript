b43bc7851a17bb9d3ecaf6c813a10545c02e123d
Vinay Thakur <thakurv69515@gmail.com>
2026-01-16 10:14:53 +0000

    conversionOperation

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,24 @@
+let score ="33abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);  //nan stand for not a number
+
+
+//"33=> 33"
+//"33abc"=> NaN (its type is number)
+// boolean true=> 1; whereas false returns =>0
+
+isLoggedIn = 1
+
+let booleanIsLoggedIn 
+
+
+
+
+
+
+
+
