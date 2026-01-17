4bf37fdae95f59520bc880b69123a9cdfbd5765d
Vinay Thakur <thakurv69515@gmail.com>
2026-01-17 05:26:21 +0000

    conversion of datatype

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -1,19 +1,32 @@
-let score ="33abc"
+// let score ="33abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);  //nan stand for not a number
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);  //nan stand for not a number
 
 
 //"33=> 33"
 //"33abc"=> NaN (its type is number)
 // boolean true=> 1; whereas false returns =>0
 
-isLoggedIn = 1
+let isLoggedIn = "Vinay"
 
-let booleanIsLoggedIn 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=> true, 0=> false....... enmpty string returns false ("")
+// & srting with value returns true("Vinay")
+
+
+let someNumber = 55
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+
+
+
+
 
 
 
