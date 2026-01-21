3a7f0362368a0e629844bd0db706e8fa6de2148c
Vinay Thakur <thakurv69515@gmail.com>
2026-01-21 06:22:09 +0000

    date

--- 01_basics/06_dates_in_js.js
+++ 01_basics/06_dates_in_js.js
@@ -6,8 +6,17 @@ let myDate = new Date()
 // console.log(myDate.toISOString());
 
 // let myNewDate = new Date(2024,1,21)// months starts from 0
-let myNewDate = new Date("2024,01,21")// we can also write in this format ,but in dual number months starts from 01
-console.log(myNewDate.toDateString());
+// let myNewDate = new Date("2024,01,21")// we can also write in this format ,but in dual number months starts from 01
+let myNewDate = new Date("01-01-2025")// Indian standards
+// console.log(myNewDate.toString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
+console.log(myNewDate.getTime());//used to compare.....
+
+
+
 
 
 
